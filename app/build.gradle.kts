3f65853d05f9fedb6a25d50762c8a6fabd1235d7
danimefalls <kondakov.d,study@gmail.com>
2025-06-21 00:31:35 +0300

    7. Дополнение библиотек в сборку

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
